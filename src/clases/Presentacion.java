68b7c97052ddffddc56f35236f5b4173ec7ba16c
AlexisRodriguez211758 <al211758@edu.uaa.mx>
2021-06-17 20:02:35 -0500

    proyecto 1.2.11

--- src/clases/Presentacion.java
+++ src/clases/Presentacion.java
@@ -27,7 +27,7 @@ public class Presentacion extends javax.swing.JFrame {
         jButtonSiguiente.setOpaque(false);
         jButtonSiguiente.setBackground(new Color(0,0,0,32));
     }
-    //igregar imagenes
+    //igregar imagenes cambio de tamaño de pixeles a 240 x 190
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
